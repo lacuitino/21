9f094037294f7638813b48925bb8fba20921825b
Lucas Cuitino <cuitino.lu@gmail.com>
2018-07-10 21:51:42 -0400

    fourth commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
   get 'pages/three'
-  get 'langings/pages'
-  root 'pages#index' #hace que la página de inicio sea index
+  root 'pages#one' #hace que la página de inicio sea index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
